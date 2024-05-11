5e5322a64cb558ea907dd553e98b4e0866640877
Tamicktom <rickpokemon38@gmail.com>
2024-05-10 21:52:55 -0300

    my solution to refactor challange 2

---
+++ aulas/02-booleans.ts
@@ -0,0 +1,14 @@
+//* Booleans
+
+const isUserOnMajority = true;
+
+// is, does, has, should, can, will, etc.
+
+const disabled = false;
+const isDisabled = true;
+
+const userHasGroup = true;
+
+if (isDisabled) {
+  console.log('The user is disabled');
+}
